445bdd7faee4d8047fd1a4c92bb95caf732ac50d
Jerin Peter <34794384+jerinpeter@users.noreply.github.com>
2021-04-25 22:13:11 +0530

    before encoder

--- ARC_2.0.ino
+++ ARC_2.0.ino
@@ -9,13 +9,15 @@
 RMCS2303 rmcs;
 byte slave_id1=1;
 byte slave_id2=2;
-int INP_CONTROL_MODE=257;           
-int PP_gain=32;
-int PI_gain=16;
-int VF_gain=32;
-int LPR=334;
-int acceleration=5000;
-int speed=8000;
+byte slave_id3=3;
+byte slave_id4=4;
+//int INP_CONTROL_MODE=257;           
+//int PP_gain=32;
+//int PI_gain=16;
+//int VF_gain=32;
+//int LPR=334;
+//int acceleration=5000;
+//int speed=8000;
 
 
 
@@ -28,7 +30,7 @@ std_msgs::Float32 l_v;
 std_msgs::Float32 r_v;
 ros::Publisher lvel("lvel", &l_v);
 ros::Publisher rvel("rvel", &r_v);
-double wheel_rad = 0.0625, wheel_sep = 0.340;
+double wheel_rad = 0.1250, wheel_sep = 0.300;
 double w_r=0, w_l=0;
 double speed_ang;
 double speed_lin;
@@ -51,6 +53,8 @@ void messageCb( const geometry_msgs::Twist& msg)
   rightPWM=0;//or break right motor
   rmcs.Disable_Digital_Mode(slave_id1,0);
   rmcs.Disable_Digital_Mode(slave_id2,0);
+  rmcs.Disable_Digital_Mode(slave_id3,0);
+  rmcs.Disable_Digital_Mode(slave_id4,0);
 //  rmcs.Disable_Digital_Mode(slave_id2,1);
 //  rmcs.Disable_Digital_Mode(slave_id2,1);
 //  rmcs.Brake_Motor(slave_id1,0);
@@ -58,12 +62,14 @@ void messageCb( const geometry_msgs::Twist& msg)
 //
 }
   else
-  rightPWM = mapFloat(fabs(w_r),0.0,8.0,2000,17000);
+  rightPWM = mapFloat(fabs(w_r),0.0,7.0,1500,17200);
   
   if(w_l==0){
   leftPWM=0; // or break left motor
   rmcs.Disable_Digital_Mode(slave_id1,0);
   rmcs.Disable_Digital_Mode(slave_id2,0);
+  rmcs.Disable_Digital_Mode(slave_id3,0);
+  rmcs.Disable_Digital_Mode(slave_id4,0);
 //  rmcs.Disable_Digital_Mode(slave_id2,1);
 //  rmcs.Disable_Digital_Mode(slave_id2,1);
 
@@ -72,59 +78,80 @@ void messageCb( const geometry_msgs::Twist& msg)
   }
   
   else
-  leftPWM = mapFloat(fabs(w_l),0.0,8.0,2000,17000);
+  leftPWM = mapFloat(fabs(w_l),0.0,8.0,1500,17200);
 
+  rmcs.Speed(slave_id1,rightPWM);
+  rmcs.Speed(slave_id2,rightPWM);
+  rmcs.Speed(slave_id3,leftPWM);
+  rmcs.Speed(slave_id4,leftPWM); 
 
 if(w_r>0 && w_l>0){
   
- rmcs.Speed(slave_id1,rightPWM);
- rmcs.Speed(slave_id2,leftPWM);                 // forward
+// rmcs.Speed(slave_id1,rightPWM);
+// rmcs.Speed(slave_id2,rightPWM);
+// rmcs.Speed(slave_id3,leftPWM); 
+// rmcs.Speed(slave_id4,leftPWM);         // forward
  rmcs.Enable_Digital_Mode(slave_id1,0);
  rmcs.Enable_Digital_Mode(slave_id2,0);
+ rmcs.Enable_Digital_Mode(slave_id3,0);
+ rmcs.Enable_Digital_Mode(slave_id4,0);
   
 }
 
 else if(w_r<0 && w_l<0){
   
- rmcs.Speed(slave_id1,rightPWM);
- rmcs.Speed(slave_id2,leftPWM);                 // backwards
+// rmcs.Speed(slave_id1,rightPWM);
+// rmcs.Speed(slave_id2,rightPWM);
+// rmcs.Speed(slave_id3,leftPWM);
+// rmcs.Speed(slave_id4,leftPWM);       // backwards
  rmcs.Enable_Digital_Mode(slave_id1,1);
  rmcs.Enable_Digital_Mode(slave_id2,1);
+ rmcs.Enable_Digital_Mode(slave_id3,1);
+ rmcs.Enable_Digital_Mode(slave_id4,1);
   
 }
 else if(w_r>0 && w_l<0){
   
- rmcs.Speed(slave_id1,rightPWM);
- rmcs.Speed(slave_id2,leftPWM);                 //Left
+// rmcs.Speed(slave_id1,rightPWM);
+// rmcs.Speed(slave_id2,rightPWM);
+// rmcs.Speed(slave_id3,leftPWM);
+// rmcs.Speed(slave_id4,leftPWM);      //Left
  rmcs.Enable_Digital_Mode(slave_id1,0);
- rmcs.Enable_Digital_Mode(slave_id2,1);
+ rmcs.Enable_Digital_Mode(slave_id2,0);
+ rmcs.Enable_Digital_Mode(slave_id3,1);
+ rmcs.Enable_Digital_Mode(slave_id4,1);
   
 }
 
 else if(w_r<0 && w_l>0){
   
- rmcs.Speed(slave_id1,rightPWM);
- rmcs.Speed(slave_id2,leftPWM);                 //Right
+// rmcs.Speed(slave_id1,rightPWM);
+// rmcs.Speed(slave_id2,rightPWM);
+// rmcs.Speed(slave_id3,leftPWM); 
+// rmcs.Speed(slave_id4,leftPWM);          //Right
  rmcs.Enable_Digital_Mode(slave_id1,1);
- rmcs.Enable_Digital_Mode(slave_id2,0);
+ rmcs.Enable_Digital_Mode(slave_id2,1);
+ rmcs.Enable_Digital_Mode(slave_id3,0);
+ rmcs.Enable_Digital_Mode(slave_id4,0);
   
 }
 
 else {
   rmcs.Brake_Motor(slave_id1,0);
   rmcs.Brake_Motor(slave_id2,0);
+  rmcs.Brake_Motor(slave_id3,0);
+  rmcs.Brake_Motor(slave_id4,0);
   rmcs.Brake_Motor(slave_id1,1);
   rmcs.Brake_Motor(slave_id2,1);
+  rmcs.Brake_Motor(slave_id3,1);
+  rmcs.Brake_Motor(slave_id4,1);
 }
   
-//  r_v.data = rightPWM;
-//  l_v.data = leftPWM;
-
-    r_v.data = w_r;
-  l_v.data = w_l;
+  r_v.data = rightPWM;
+  l_v.data = leftPWM;
 
 
-
+  
 
 }
 ros::Subscriber<geometry_msgs::Twist> sub("cmd_vel", &messageCb );
